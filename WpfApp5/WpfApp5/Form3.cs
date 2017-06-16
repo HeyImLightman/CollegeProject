0836e62469cc0c938b190305d5d10b35d2e7fb77
vantasicom <vantasicom.a@ya.ru>
2017-06-16 23:34:12 +0700

    Щ9Щ

---
+++ WpfApp5/WpfApp5/Form3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfApp5
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void pupilsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pupilsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.schoolDataSet);
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "schoolDataSet.Pupils". При необходимости она может быть перемещена или удалена.
+            this.pupilsTableAdapter.Fill(this.schoolDataSet.Pupils);
+
+        }
+    }
+}
